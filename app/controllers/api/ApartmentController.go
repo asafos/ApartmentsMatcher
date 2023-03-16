ec7f8311b96bb95fdfc3e3e73ec017708963c5a9
asafos <yehezkel6@gmail.com>
2023-03-16 12:06:00 +0200

    Added ApartmentService

--- app/controllers/api/ApartmentController.go
+++ app/controllers/api/ApartmentController.go
@@ -3,6 +3,7 @@ package api
 import (
 	utils "fiber-boilerplate/app/controllers/utils"
 	"fiber-boilerplate/app/models"
+	services "fiber-boilerplate/app/services/api"
 	"fiber-boilerplate/database"
 
 	"github.com/gofiber/fiber/v2"
@@ -12,7 +13,7 @@ import (
 func GetAllApartments(db *database.Database) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
 		var Apartments []models.Apartment
-		if response := db.Preload("ApartmentPrefs").Preload("Apartments").Find(&Apartments); response.Error != nil {
+		if response := services.GetAllApartments(db, &Apartments); response.Error != nil {
 			return utils.SendError(ctx, "Error occurred while retrieving apartments from the database: "+response.Error.Error(), fiber.StatusInternalServerError)
 		}
 		err := ctx.JSON(Apartments)
@@ -28,7 +29,7 @@ func GetApartment(db *database.Database) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
 		Apartment := new(models.Apartment)
 		id := ctx.Params("id")
-		if response := db.Find(&Apartment, id); response.Error != nil {
+		if response := services.GetApartment(db, Apartment, id); response.Error != nil {
 			return utils.SendError(ctx, "An error occurred when retrieving the apartment: "+response.Error.Error(), fiber.StatusInternalServerError)
 		}
 		if Apartment.ID == 0 {
@@ -59,7 +60,7 @@ func AddApartment(db *database.Database) fiber.Handler {
 		if err := ctx.BodyParser(Apartment); err != nil {
 			return utils.SendError(ctx, "an error occurred when parsing the new apartment", fiber.StatusBadRequest)
 		}
-		if response := db.Create(&Apartment); response.Error != nil {
+		if response := services.AddApartment(db, Apartment); response.Error != nil {
 			return utils.SendError(ctx, "an error occurred when storing the new apartment"+response.Error.Error(), fiber.StatusInternalServerError)
 		}
 		err := ctx.JSON(Apartment)
@@ -79,7 +80,7 @@ func EditApartment(db *database.Database) fiber.Handler {
 		if err := ctx.BodyParser(EditApartment); err != nil {
 			return utils.SendError(ctx, "An error occurred when parsing the edited apartment: "+err.Error(), fiber.StatusBadRequest)
 		}
-		if response := db.Find(&Apartment, id); response.Error != nil {
+		if response := services.GetApartment(db, Apartment, id); response.Error != nil {
 			return utils.SendError(ctx, "An error occurred when retrieving the existing apartment: "+response.Error.Error(), fiber.StatusNotFound)
 		}
 		// Apartment does not exist
@@ -108,7 +109,7 @@ func EditApartment(db *database.Database) fiber.Handler {
 		Apartment.Location = EditApartment.Location
 
 		// Save apartment
-		db.Save(&Apartment)
+		services.EditApartment(db, Apartment)
 
 		err := ctx.JSON(Apartment)
 		if err != nil {
@@ -123,11 +124,10 @@ func DeleteApartment(db *database.Database) fiber.Handler {
 	return func(ctx *fiber.Ctx) error {
 		id := ctx.Params("id")
 		var Apartment models.Apartment
-		db.Find(&Apartment, id)
 		if response := db.Find(&Apartment); response.Error != nil {
 			return utils.SendError(ctx, "An error occurred when finding the apartment to be deleted"+response.Error.Error(), fiber.StatusNotFound)
 		}
-		db.Delete(&Apartment)
+		services.DeleteApartment(db, &Apartment)
 
 		err := ctx.JSON(fiber.Map{
 			"ID":      id,
---
+++ app/services/api/ApartmentService.go
@@ -0,0 +1,28 @@
+package serviceApi
+
+import (
+	"fiber-boilerplate/app/models"
+	"fiber-boilerplate/database"
+
+	"gorm.io/gorm"
+)
+
+func GetAllApartments(db *database.Database, dest *[]models.Apartment) *gorm.DB {
+	return db.Find(&dest)
+}
+
+func GetApartment(db *database.Database, dest *models.Apartment, id string) *gorm.DB {
+	return db.Find(&dest, id)
+}
+
+func AddApartment(db *database.Database, dest *models.Apartment) *gorm.DB {
+	return db.Create(dest)
+}
+
+func EditApartment(db *database.Database, dest *models.Apartment) *gorm.DB {
+	return db.Save(dest)
+}
+
+func DeleteApartment(db *database.Database, dest *models.Apartment) *gorm.DB {
+	return db.Delete(dest)
+}
