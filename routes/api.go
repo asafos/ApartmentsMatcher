2b19511e8a8c0a62ff5be399463b1320e5222052
asafos <yehezkel6@gmail.com>
2023-03-17 18:07:34 +0200

    Exposed endpoints

--- routes/api.go
+++ routes/api.go
@@ -10,6 +10,8 @@ import (
 func RegisterAPI(api fiber.Router, db *database.Database) {
 	registerRoles(api, db)
 	registerUsers(api, db)
+	registerApartments(api, db)
+	registerApartmentPrefs(api, db)
 }
 
 func registerRoles(api fiber.Router, db *database.Database) {
@@ -30,4 +32,30 @@ func registerUsers(api fiber.Router, db *database.Database) {
 	users.Post("/", Controller.AddUser(db))
 	users.Put("/:id", Controller.EditUser(db))
 	users.Delete("/:id", Controller.DeleteUser(db))
+}
+
+func registerApartments(api fiber.Router, db *database.Database) {
+	users := api.Group("/apartments")
+
+	users.Get("/", Controller.GetAllApartments(db))
+	users.Get("/:id", Controller.GetApartment(db))
+	users.Post("/", Controller.AddApartment(db))
+	users.Put("/:id", Controller.EditApartment(db))
+	users.Delete("/:id", Controller.DeleteApartment(db))
+}
+
+func registerApartmentPrefs(api fiber.Router, db *database.Database) {
+	users := api.Group("/apartmentPrefs")
+
+	users.Get("/", Controller.GetAllApartmentPrefs(db))
+	users.Get("/:id", Controller.GetApartmentPref(db))
+	users.Post("/", Controller.AddApartmentPref(db))
+	users.Put("/:id", Controller.EditApartmentPref(db))
+	users.Delete("/:id", Controller.DeleteApartmentPref(db))
+}
+
+func registerMatching(api fiber.Router, db *database.Database) {
+	users := api.Group("/match")
+
+	users.Get("/user/:id", Controller.GetMatchingApartments(db))
 }
