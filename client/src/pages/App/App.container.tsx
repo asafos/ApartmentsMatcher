395ecb4a72373e8d22df4517cbaa297c93224bfb
asafos <yehezkel6@gmail.com>
2023-03-24 09:56:02 +0300

    Moved routing to App.tsx

--- client/src/pages/App/App.container.tsx
+++ client/src/pages/App/App.container.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-import { redirect, useLocation, useNavigate } from "react-router-dom";
 import { useAuthStore } from "../../DAL/stores/auth";
 import { DataObjectState } from "../../DAL/stores/types";
 import { App } from "./App";
@@ -7,8 +6,7 @@ import { App } from "./App";
 function AppContainer() {
     const fetchUser = useAuthStore((state) => state.fetchUser)
     const user = useAuthStore((state) => state.user)
-    const navigate = useNavigate();
-    const location = useLocation();
+
 
     useEffect(() => {
         if (user.state === DataObjectState.NotStarted) {
@@ -16,18 +14,8 @@ function AppContainer() {
         }
     }, [user])
 
-    if (user.state === DataObjectState.InProgress || user.state === DataObjectState.NotStarted) {
-        return <div>Loading...</div>
-    }
-
-    if (user.state === DataObjectState.Failed) {
-        const from = location.state?.from?.pathname || "/";
-        navigate("/login", { state: { from } });
-        return null
-    }
-
     return (
-        <App />
+        <App user={user} />
     );
 }
 
--- client/src/pages/App/App.tsx
+++ client/src/pages/App/App.tsx
@@ -1,8 +1,28 @@
-import { useRoutes } from "react-router-dom";
+import { useLocation, useNavigate, useRoutes } from "react-router-dom";
+import { User } from "../../DAL/stores/auth";
+import { DataObject, DataObjectState } from "../../DAL/stores/types";
 import { appRoutes } from "../../routes";
 
-function App() {
+type Props = {
+    user: DataObject<User | null>
+}
+
+function App(props: Props) {
+    const { user } = props
     const routeResult = useRoutes(appRoutes);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    if (user.state === DataObjectState.InProgress || user.state === DataObjectState.NotStarted) {
+        return <div>Loading...</div>
+    }
+
+    if (user.state === DataObjectState.Failed) {
+        const from = location.state?.from?.pathname || "/";
+        navigate("/login", { state: { from } });
+        return null
+    }
+
     return (
         <>
             {routeResult}
