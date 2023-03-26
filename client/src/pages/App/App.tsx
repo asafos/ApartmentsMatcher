8e54beb1f744225163fa3652d66ed2411ed1ec33
asafos <yehezkel6@gmail.com>
2023-03-26 21:48:33 +0300

    Fixed routing

--- client/src/pages/App/App.tsx
+++ client/src/pages/App/App.tsx
@@ -2,43 +2,22 @@ import { useEffect } from "react";
 import { useLocation, useNavigate, useRoutes } from "react-router-dom";
 import { Apartment } from "../../DAL/services/apartments/apartments";
 import { User } from "../../DAL/services/auth/auth";
-import { DataObject, DataObjectState } from "../../DAL/stores/types";
 import { appRoutes } from "../../routes";
 
 type Props = {
-    user: DataObject<User | null>
-    apartment: DataObject<Apartment | null>
+    user: User | null
+    isLoading: boolean
 }
 
 function App(props: Props) {
-    const { user, apartment } = props
+    const { user, isLoading } = props
     const routeResult = useRoutes(appRoutes);
-    const navigate = useNavigate();
-    const location = useLocation();
 
-    useEffect(() => {
-        if (user.state === DataObjectState.Failed) {
-            const from = location.state?.from?.pathname || "/";
-            navigate("/login", { state: { from } });
-        }
-    }, [user])
-
-    useEffect(() => {
-        if (apartment.state === DataObjectState.Succeeded && !apartment.data) {
-            navigate("/apartment/add");
-        }
-    }, [apartment])
-
-    if (
-        user.state === DataObjectState.Fetching
-        || user.state === DataObjectState.NotStarted
-        || apartment.state === DataObjectState.NotStarted
-        || apartment.state === DataObjectState.Fetching
-    ) {
+    if (isLoading) {
         return <div>Loading...</div>
     }
 
-    if (user.state === DataObjectState.Failed) {
+    if (!user) {
         return null
     }
 
